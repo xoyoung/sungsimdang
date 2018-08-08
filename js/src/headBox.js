31cab42dacdafecee9ddbabbb408fa68216ae170
xoyoung <starkimso@gmail.com>
2018-08-08 15:42:26 +0900

    0808_sungsimdang

--- js/src/headBox.js
+++ js/src/headBox.js
@@ -7,12 +7,18 @@
   var gnb = $('.gnb');
   var gnbUl = gnb.children('ul');
   var gnbMenu = gnbUl.children('li');
+  var under = gnbMenu.children('div');
+  var underUl = under.children('ul');
+  var underM = underUl.children('li');
 
   var login = $('.login').children('.sub');
   var loginP = $('.login_pop');
 
   var cart = $('.cart');
   var cartP = $('.cart_pop');
+
+  var link = ['./p1_ssd_about.html','./p1_ssd_brand.html','./p1_ssd_shop.html','./p1_ssd_news.html','./p1_ssd_help.html'];
+
 
  // mobile&tab 
  
@@ -41,7 +47,15 @@
   
     gnbMenu.on('click',function(e){
      e.preventDefault();
-
+     var i = $(this).index();
+     console.log(i);
+     console.log(link[i]);
+     
+     under.eq(i).find(underM).on('click',function(){
+      $(location).attr('href',link[i]);
+     });
+     
+   
    var thisNext = $(this).children('div').css('display');
 
     if(thisNext === 'block'){
@@ -66,6 +80,10 @@ $(window).on('resize',function(e){
   }
   
 });
+
+
+
+
 
 
 
